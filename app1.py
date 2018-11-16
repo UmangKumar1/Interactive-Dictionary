89fda18df23ca55d32e97bed23a19751d6237d02
Umang Kumar <ukuma001@ucr.edu>
2018-11-15 16:19:30 -0800

    I added a function that finds the correct word the user meant by amtching it to all the key values in our dictionary

--- app1.py
+++ app1.py
@@ -1,4 +1,8 @@
 import json
+import difflib
+
+from difflib import SequenceMatcher
+from difflib import get_close_matches
 
 inputName = input("Which word would you like the definition for?  ")
 inputName = inputName.lower()
@@ -9,7 +13,21 @@ def translate(word):
     if word in data:
         return data[word]
     else:
-        return "Word is not in the dictionary"
+
+        closeWord = get_close_matches(word,data.keys(), n = 10)[0]
+        changeName = input("That is a not a word, are you sure you didnt mean:: " + closeWord + " ?? (Enter 1 for yes) :")
+        if changeName == "1":
+            return data[closeWord]
+        else:
+            while changeName != "1":
+                x = 0
+                closeWord = get_close_matches(word,data.keys(), n=10)[x]
+                x = x+1
+                changeName = input("What about: " + closeWord + " ?? (Enter 1 for yes) :")
+            return data[closeWord]
+
+
+
 
 
 print(translate(inputName))
